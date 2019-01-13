032bfddd8116d18fca776598223968c2b12659ad
nafrid <irfan.daval@gmail.com>
2019-01-13 13:14:26 +0100

    removal of git config details

--- jenkinsdsl.groovy
+++ jenkinsdsl.groovy
@@ -3,8 +3,6 @@ job('Jenkins example') {
   def repo = 'https://github.com/nafridirfan/Jenkins.git'
     scm {
         git(repo) {
-            gitConfigName('nafrid')
-            gitConfigEmail('irfan.daval@gmail.com')
             branches('dev')
             scriptPath('Jenkinsfile')
 

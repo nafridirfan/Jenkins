861730e98a8901d2ffdcd23c35efc56e34efe000
nafridirfan <irfan.daval@gmail.com>
2019-01-13 13:10:46 +0100

    adding username and email of git

--- jenkinsdsl.groovy
+++ jenkinsdsl.groovy
@@ -1,8 +1,13 @@
 job('Jenkins example') {
+
+  def repo = 'https://github.com/nafridirfan/Jenkins.git'
     scm {
-        git('https://github.com/nafridirfan/Jenkins.git') {
-        branches('dev')
-        scriptPath('Jenkinsfile')
+        git(repo) {
+            gitConfigName('nafrid')
+            gitConfigEmail('irfan.daval@gmail.com')
+            branches('dev')
+            scriptPath('Jenkinsfile')
+
         }
     }
     triggers {
